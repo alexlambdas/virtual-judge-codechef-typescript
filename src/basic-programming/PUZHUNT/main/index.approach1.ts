45b1189b26c8811d13514df2ee87fe88162db106
alexLambdas <alexlambdas@gmail.com>
2023-07-28 00:50:31 -0500

    feat: codechef PUZHUNT

---
+++ src/basic-programming/PUZHUNT/main/index.approach1.ts
@@ -0,0 +1,21 @@
+//
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+//
+let input: string = '';
+process.stdin.on('data', (chunk) => input += chunk);
+process.stdin.on('end', () => entryPoint(input.toString().split('\n')));
+
+function puzzleHunt(dataIn: string[]): string{
+  const teamLength: number = parseInt(dataIn[0]);
+  if(teamLength >= 6 && teamLength <= 8) return 'Yes';
+  else return 'No';
+}
+
+function entryPoint(dataIn: string[]): void{
+  const result: string = puzzleHunt(dataIn);
+  console.log(result);
+}
+
+entryPoint(Array('8'));
